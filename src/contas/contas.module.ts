80cedd6a8dc7183e9d9550f9a9bddb26caaaa11d
Arthur Silva <arthur.gbs.dev@gmail.com>
2026-02-15 23:05:03 -0300

    Pre ajudando o import do module

--- src/contas/contas.module.ts
+++ src/contas/contas.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ContasService } from './contas.service';
 import { ContasController } from './contas.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Conta } from './entities/conta.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Conta])],
   controllers: [ContasController],
   providers: [ContasService],
 })
