ed098208828b952284017889a1d9d39061aaf69d
Arthur Silva <arthurborges2607@gmail.com>
2026-02-22 12:17:18 -0300

    Ajustando metodo createTransfer, adicionando um dataSource com transaction

--- src/transacoes/transacoes.service.ts
+++ src/transacoes/transacoes.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateTransacoeDto } from './dto/create-transacoe.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Transacao } from './entities/transacao.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Conta } from 'src/contas/entities/conta.entity';
 
 @Injectable()
@@ -12,6 +12,7 @@ export class TransacoesService {
     private readonly transacaoRepository: Repository<Transacao>,
     @InjectRepository(Conta)
     private readonly contaRepository: Repository<Conta>,
+    private dataSource: DataSource,
   ) {}
 
   async createDeposit(dto: CreateTransacoeDto) {
@@ -44,7 +45,7 @@ export class TransacoesService {
     return this.transacaoRepository.save(newTransacao);
   }
 
-  async createTransfer(dto: CreateTransacoeDto) {
+  async createTransfer1(dto: CreateTransacoeDto) {
     const contaOrigem = await this.contaRepository.findOneBy({
       id: dto.contaOrigemId,
     });
@@ -52,7 +53,7 @@ export class TransacoesService {
       throw new Error('Account not found');
     }
     const contaDestino = await this.contaRepository.findOneBy({
-      id: dto.contaOrigemId,
+      id: dto.contaDestinoId,
     });
     if (!contaDestino) {
       throw new Error('Account not found');
@@ -64,7 +65,36 @@ export class TransacoesService {
     });
     contaOrigem.balance -= dto.valor;
     contaDestino.balance += dto.valor;
+    await this.contaRepository.save(contaOrigem);
+    await this.contaRepository.save(contaDestino);
     return this.transacaoRepository.save(newTransacao);
+  }
+
+  async createTransfer(dto: CreateTransacoeDto) {
+    return await this.dataSource.transaction(async (manager) => {
+      const contaOrigem = await this.contaRepository.findOneBy({
+        id: dto.contaOrigemId,
+      });
+      if (!contaOrigem) {
+        throw new Error('Account not found');
+      }
+      const contaDestino = await this.contaRepository.findOneBy({
+        id: dto.contaDestinoId,
+      });
+      if (!contaDestino) {
+        throw new Error('Account not found');
+      }
+      contaOrigem.balance -= dto.valor;
+      contaDestino.balance += dto.valor;
+      await manager.save(contaOrigem);
+      await manager.save(contaDestino);
+      const newTransacao = manager.create(Transacao, {
+        contaOrigem: contaOrigem,
+        contaDestino: contaDestino,
+        valor: dto.valor,
+      });
+      await manager.save(newTransacao);
+    });
   }
 
   async findOne(id: string) {
